bb5f309b731b851178ecf1965c2d60cd62b7c901
Yuning Shi <shiyuning@gmail.com>
2019-10-20 15:20:12 -0400

    :zap: Code optimization from @mayani

--- GLDAS-Cycles-transformation.py
+++ GLDAS-Cycles-transformation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import argparse
 import math
-import sys
 import os
+import shutil
 import numpy as np
-from datetime import timedelta, date, datetime
+from datetime import datetime, timedelta
 from netCDF4 import Dataset
+
 
 def Closest(lat, lon, path):
 
@@ -15,19 +17,18 @@ def Closest(lat, lon, path):
     best_y = (np.abs(nc.variables['lat'][:] - lat)).argmin()
     best_x = (np.abs(nc.variables['lon'][:] - lon)).argmin()
 
-    return (best_y, best_x, nc['lat'][best_y], nc['lon'][best_x], nc['GLDAS_elevation'][0, best_y, best_x])
+    return (best_y, best_x, nc["lat"][best_y], nc["lon"][best_x],
+            nc["GLDAS_elevation"][0, best_y, best_x])
 
 
 def ReadVar(y, x, nc_name):
-
-    nc = Dataset(nc_name, 'r')
-
-    _prcp = nc['Rainf_f_tavg'][0, y, x]
-    _temp = nc['Tair_f_inst'][0, y, x]
-    _wind = nc['Wind_f_inst'][0, y, x]
-    _solar = nc['SWdown_f_tavg'][0, y, x]
-    _pres = nc['Psurf_f_inst'][0, y, x]
-    _spfh = nc['Qair_f_inst'][0, y, x]
+    with Dataset(nc_name, 'r') as nc:
+        _prcp = nc['Rainf_f_tavg'][0, y, x]
+        _temp = nc['Tair_f_inst'][0, y, x]
+        _wind = nc['Wind_f_inst'][0, y, x]
+        _solar = nc['SWdown_f_tavg'][0, y, x]
+        _pres = nc['Psurf_f_inst'][0, y, x]
+        _spfh = nc['Qair_f_inst'][0, y, x]
 
     es = 611.2 * math.exp(17.67 * (_temp - 273.15) / (_temp - 273.15 + 243.5))
     ws = 0.622 * es / (_pres - es)
@@ -38,8 +39,9 @@ def ReadVar(y, x, nc_name):
 
     return (_prcp, _temp, _wind, _solar, _rh)
 
+
 def satvp(temp):
-    return .6108 * math.exp(17.27 * temp / (temp + 237.3))
+    return 0.6108 * math.exp(17.27 * temp / (temp + 237.3))
 
 
 def ea(patm, q):
@@ -48,7 +50,7 @@ def ea(patm, q):
 
 def process_day(t, y, x, path):
     '''
-    process one day of GLDAS data and convert it to Cycles input
+    Process one day of GLDAS data and convert it to Cycles input
     '''
 
     prcp = 0.0
@@ -60,14 +62,15 @@ def process_day(t, y, x, path):
     rhn = 999.0
     counter = 0
 
-    print(datetime.strftime(t, "%Y-%m-%d"))
+    print(datetime.strftime(t, '%Y-%m-%d'))
 
-    nc_path = '%s/%4.4d/%3.3d/' %(path, t.timetuple().tm_year, t.timetuple().tm_yday)
+    nc_path = '%s/%4.4d/%3.3d/' % (path, t.timetuple().tm_year,
+                                   t.timetuple().tm_yday)
 
     for nc_name in os.listdir(nc_path):
-        if nc_name.endswith(".nc4"):
-            nc = Dataset(os.path.join(nc_path, nc_name), 'r')
-            (_prcp, _temp, _wind, _solar, _rh) = ReadVar(y, x, os.path.join(nc_path, nc_name))
+        if nc_name.endswith('.nc4'):
+            (_prcp, _temp, _wind, _solar, _rh) = ReadVar(
+                y, x, os.path.join(nc_path, nc_name))
 
             prcp += _prcp
 
@@ -95,7 +98,7 @@ def process_day(t, y, x, path):
     wind /= float(counter)
 
     solar /= float(counter)
-    solar *= 86400.0 / 1.0E6
+    solar *= 86400.0 / 1.0e6
 
     rhx *= 100.0
     rhn *= 100.0
@@ -103,63 +106,114 @@ def process_day(t, y, x, path):
     tx -= 273.15
     tn -= 273.15
 
-    data = '%-16s%-8.4f%-8.2f%-8.2f%-8.4f%-8.2f%-8.2f%-8.2f\n' \
-       %(t.strftime('%Y    %j'), prcp, tx, tn, solar, rhx, rhn, wind)
+    data = "%-16s%-8.4f%-8.2f%-8.2f%-8.4f%-8.2f%-8.2f%-8.2f\n" % (
+        t.strftime("%Y    %j"), prcp, tx, tn, solar, rhx, rhn, wind)
 
     return data
 
 
-def main():
+def main(start_date, end_date, gldas_path, output_prefix,
+         latitude=None, longitude=None, coord_file=None):
 
-    if (len(sys.argv) != 4):
-        print("Illegal number of parameters.")
-        print("Usage: GLDAS-Cycles-transformation.py YYYY-MM-DD YYYY-MM-DD DATA_PATH")
-        sys.exit(0)
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    data_path = gldas_path
 
-    start_date = datetime.strptime(sys.argv[1], "%Y-%m-%d")
-    end_date = datetime.strptime(sys.argv[2], "%Y-%m-%d")
-    data_path = sys.argv[3]
-
-    filepath = 'location.txt'
-    with open(filepath) as fp:
-        for cnt, line in enumerate(fp):
-            li=line.strip()
-            if not (li.startswith("#") or li.startswith("L")):
+    if latitude and longitude:
+        coords = [(latitude, longitude, output_prefix)]
+    elif coord_file:
+        coords = []
+        with open(coord_file) as fp:
+            for cnt, line in enumerate(fp):
+                li = line.strip()
+                if not (li.startswith('#') or li.startswith('L')):
                     nums = line.split()
                     lat = float(nums[0])
                     lon = float(nums[1])
+                    coords.append((lat, lon, "%s-%d.weather" % (
+                        output_prefix, cnt)))
+    else:
+        raise ValueError("Invalid coordinates")
 
-                    print('Processing data for {0}, {1}'.format(lat, lon))
+    memoize = {}
+    for lat, lon, fname in coords:
+        print("Processing data for {0}, {1}".format(lat, lon))
 
-                    (y, x, grid_lat, grid_lon, elevation) = Closest(lat, lon, data_path)
+        (y, x, grid_lat, grid_lon, elevation) = Closest(lat, lon, data_path)
 
-                    if grid_lat < 0.0:
-                        lat_str = '%.2fS' %(abs(grid_lat))
-                    else:
-                        lat_str = '%.2fN' %(abs(grid_lat))
+        if grid_lat < 0.0:
+            lat_str = "%.2fS" % (abs(grid_lat))
+        else:
+            lat_str = "%.2fN" % (abs(grid_lat))
 
-                    if grid_lon < 0.0:
-                        lon_str = '%.2fW' %(abs(grid_lon))
-                    else:
-                        lon_str = '%.2fE' %(abs(grid_lon))
+        if grid_lon < 0.0:
+            lon_str = "%.2fW" % (abs(grid_lon))
+        else:
+            lon_str = "%.2fE" % (abs(grid_lon))
 
-                    fname = 'met' + lat_str + 'x' + lon_str + '.weather'
-                    outfp = open(fname, 'w')
-                    outfp.write('LATITUDE %.2f\n' %(grid_lat))
-                    outfp.write('ALTITUDE %.2f\n' %(elevation))
-                    outfp.write('SCREENING_HEIGHT 2\n')
-                    outfp.write('YEAR    DOY     PP      TX      TN     SOLAR      RHX      RHN     WIND\n')
+        if (lat_str, lon_str) in memoize:
+            shutil.copyfile(memoize[(lat_str, lon_str)], fname)
+            continue
+        else:
+            memoize[(lat_str, lon_str)] = fname
 
-                    cday = start_date
+        # fname = "met" + lat_str + "x" + lon_str + ".weather"
+        outfp = open(fname, 'w')
+        outfp.write('LATITUDE %.2f\n' % (grid_lat))
+        outfp.write('ALTITUDE %.2f\n' % (elevation))
+        outfp.write('SCREENING_HEIGHT 2\n')
+        outfp.write(
+            'YEAR    DOY     PP      TX      TN     SOLAR      RHX      RHN     WIND\n'
+        )
 
-                    while cday <= end_date:
-                        outfp.write(process_day(cday, y, x, data_path))
-                        cday += timedelta(days = 1)
+        cday = start_date
 
-                    outfp.close()
+        while cday <= end_date:
+            outfp.write(process_day(cday, y, x, data_path))
+            cday += timedelta(days=1)
 
-    fp.close
+        outfp.close()
 
 
-main()
+def _main():
+    parser = argparse.ArgumentParser(description='GLDAS to Cycles Transform')
+    parser.add_argument('-s', '--start-date', dest='start_date', required=True,
+                        default='2000-01-01', help='Start Date',
+    )
+    parser.add_argument(
+        '-e',
+        '--end-date',
+        dest='end_date',
+        required=True,
+        default='2018-01-31',
+        help='End Date',
+    )
+    parser.add_argument(
+        '-g',
+        '--gldas-path',
+        dest='gldas_path',
+        required=True,
+        help='Path to GLDAS files',
+    )
+    parser.add_argument(
+        '-lat', '--latitude', type=float, dest='latitude', help='Latitude'
+    )
+    parser.add_argument(
+        '-lon', '--longitude', type=float, dest='longitude', help='Longitude'
+    )
+    parser.add_argument(
+        '-c',
+        '--coord-file',
+        dest='coord_file',
+        help='File with list of Latitude, Longitude pairs.',
+    )
+    parser.add_argument(
+        '-o', '--output', dest='output_prefix', required=True, help='Output prefix'
+    )
 
+    args = parser.parse_args()
+    main(**vars(args))
+
+
+if __name__ == '__main__':
+    _main()
